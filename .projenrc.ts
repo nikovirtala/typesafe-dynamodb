f7a36559b5d4044172c9cbad179662e998c07f6a
Niko Virtala <niko.virtala@sok.fi>
2025-10-15 09:41:23 +0300

    chore: remove unnecessary dom from tsconfig

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,30 +1,25 @@
-import { typescript } from 'projen';
+import { typescript } from "projen";
 
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'typesafe-dynamodb',
+  defaultReleaseBranch: "main",
+  name: "typesafe-dynamodb",
   projenrcTs: true,
-  typescriptVersion: 'latest',
-  repository: 'https://github.com/sam-goodwin/typesafe-dynamodb',
+  typescriptVersion: "latest",
+  repository: "https://github.com/sam-goodwin/typesafe-dynamodb",
   peerDeps: [
-    'aws-sdk',
-    '@aws-sdk/client-dynamodb',
-    '@aws-sdk/lib-dynamodb',
-    '@aws-sdk/types',
-    '@aws-sdk/util-dynamodb',
-    '@smithy/smithy-client',
-    '@types/aws-lambda',
+    "aws-sdk",
+    "@aws-sdk/client-dynamodb",
+    "@aws-sdk/lib-dynamodb",
+    "@aws-sdk/types",
+    "@aws-sdk/util-dynamodb",
+    "@smithy/smithy-client",
+    "@types/aws-lambda",
   ],
   eslintOptions: {
     dirs: [],
-    ignorePatterns: ['**'],
+    ignorePatterns: ["**"],
   },
-  tsconfig: {
-    compilerOptions: {
-      lib: ['dom'],
-    },
-  },
-  gitignore: ['.DS_Store', '.dccache'],
+  gitignore: [".DS_Store", ".dccache"],
   releaseToNpm: true,
 });
 
